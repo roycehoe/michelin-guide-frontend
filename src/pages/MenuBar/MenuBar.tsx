74074c53fbbd351ce3fd6facb1dae9e193ed2e70
roycehoe <royceinbox@hotmail.com>
2023-02-01 09:21:52 +0800

    Install icon material package; Add barebone sort and filter buttons to menu bar

--- src/pages/MenuBar/MenuBar.tsx
+++ src/pages/MenuBar/MenuBar.tsx
@@ -1,26 +1,27 @@
-import { MichelinMetadataResponse } from "@/services/getMichelinMetadata";
-import React, { useEffect, useState } from "react";
+import { FilterAlt } from "@mui/icons-material";
+import Sort from "@mui/icons-material/Sort";
+import { Button } from "@mui/material";
 
-import { useMenuBar } from "./useMenuBar";
+import React from "react";
 
 export const MenuBar: React.FC = () => {
-  const [menuBar, setMenuBar] = useState<MichelinMetadataResponse>([
-    null,
-  ] as MichelinMetadataResponse);
+  // const [menuBar, setMenuBar] = useState<MichelinMetadataResponse>([
+  //   null,
+  // ] as MichelinMetadataResponse);
 
-  const { getSortData } = useMenuBar();
+  // const { getSortData } = useMenuBar();
 
-  useEffect(() => {
-    const fetchSortData = async () => {
-      const data = await getSortData("coordinates");
-      if (data) {
-        setMenuBar(data);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchSortData = async () => {
+  //     const data = await getSortData("coordinates");
+  //     if (data) {
+  //       setMenuBar(data);
+  //     }
+  //   };
 
-    console.log(menuBar);
-    fetchSortData().catch(console.error);
-  }, []);
+  //   console.log(menuBar);
+  //   fetchSortData().catch(console.error);
+  // }, []);
 
   // React.useEffect(() => {
   //   const fetchData = async () => {
@@ -35,6 +36,25 @@ export const MenuBar: React.FC = () => {
   //   fetchData().catch(console.error);
   //   console.log(menuBar);
   // }, []);
+  const handleFilterClick = () => {
+    console.log("Filter click handled");
+  };
+  const handleSortClick = () => {
+    console.log("Sort click handled");
+  };
 
-  return <div className="bg-red-300">HELLO</div>;
+  return (
+    <div className="menu bg-gray-100 flex justify-end">
+      <div className="menu__filter m-2">
+        <Button variant="outlined" className="">
+          <FilterAlt onClick={handleFilterClick}></FilterAlt>
+        </Button>
+      </div>
+      <div className="menu__sort m-2 ">
+        <Button variant="outlined">
+          <Sort onClick={handleSortClick}></Sort>
+        </Button>
+      </div>
+    </div>
+  );
 };
